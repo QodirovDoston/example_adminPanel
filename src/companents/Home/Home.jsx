962fe0a3b63cd9cb4aefea29e16ff84c0c158765
qodirovdoston806 <qodirovdoston806@gmail.com>
2023-04-10 15:52:41 +0500

    fdf

--- src/companents/Home/Home.jsx
+++ src/companents/Home/Home.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import { Container, Row } from 'react-bootstrap'
 
 import { useTranslation } from 'react-i18next';
-// import i18next from '../../i18n';
+import i18next from 'i18next';
+console.log(i18next);
 
 const Home = () => {
   const { t } = useTranslation()
@@ -11,7 +12,9 @@ const Home = () => {
       <Container>
         <Row className='justify-content-center mt-5'>
            <h2 className='justify-center sm:text-[43px] text-[19px] '>{t('text.salom')}</h2>
-          <p className='sm:text-[23px] text-[10px]'>{t('text.text_p')}</p>
+          <p className='sm:text-[23px] text-[10px]   first-line:uppercase first-line:tracking-widest
+  first-letter:text-7xl first-letter:font-bold first-letter:text-slate-900
+  first-letter:mr-3 first-letter:float-left'>{t('text.text_p')}</p>
         </Row>
       </Container>
     </div>
--- src/companents/Table/TableBase.jsx
+++ src/companents/Table/TableBase.jsx
@@ -4,9 +4,9 @@ import axios from 'axios'
 import '../Table/TableBase.css'
 import { Link } from 'react-router-dom'
 
-
 import { useTranslation } from 'react-i18next';
-// import i18next from '../../i18n';
+// import i18next from 'i18next';
+
 const TableBase = () => {
   const { t } = useTranslation()
 
@@ -39,59 +39,59 @@ const TableBase = () => {
       <Container >
         <div className='justify-between '>
           <Row className='justify-content-center mt-5'>
-          <Col xs={10} sm={12}>
-            <Form.Group className=" justify-content-center mb-[-40px]">
-              <div className='text-[16px]'>{t("text.search")}</div>
-              <Form.Control placeholder="Search" onChange={(e) => SetValue(e.target.value)} />
-            </Form.Group>
-          </Col>
+            <Col xs={10} sm={12}>
+              <Form.Group className=" justify-content-center mb-[-40px]">
+                <div className='text-[16px]'>{t("text.search")}</div>
+                <Form.Control placeholder="Search" onChange={(e) => SetValue(e.target.value)} />
+              </Form.Group>
+            </Col>
           </Row>
         </div>
 
-        <div className='justify-center mt-5'  >
-          <div className=''>
-            <div className='sm:flex  grid gap-2 grid-cols-1 grid-rows-3 '>
-              {name.filter((res) => {
-                console.log(res.title);
-                return res.title.toLocaleLowerCase() === "" ? value : res.title.toLocaleLowerCase().includes(value)
-              })
-                .map((data) => {
-                  return (
-                    <>
-                      <tbody className=' sm:mt-0 sm:mb-0 mb-[-850px]  mt-[-70px] '>
-                        <tr className='m-12 sm:grid ml-0  sm:grid-cols-3 grid grid-cols-1 grid-rows-3' >
-                          <div className='bg-blue-200 mt-6 w-[350px] h-[370px] rounded-[30px] ' key={data.id}>
-                            <div className=' flex-wrap m-9  '>
-                              <th className='mb-[-20px]' >#:</th>
-                              <td  >{data.id}</td>
-                              <h r />
-                              <th >title: </th>
-                              <td  >{data.title}</td>
-                              <h r />
-                              <th >price: </th>
-                              <td  >{data.price}</td>
-                              <h r />
-                              <th >author: </th>
-                              <td >{data.author}</td>
-                              <h r />
-                              <th  >img: 
-                              </th>
-                              <img className='w-[90px] h-[90px]' src={data.img} alt="" />
-                              <div className="flex gap-3 mt-2 ">
-                                <button className='bg-blue-500 text-white w-20 h-8 rounded-3xl' onClick={() => delete_func(data.id)}>delet</button>
-                                <Link to={`/table/editetable/${data.id}`}>
-                                  <button className='bg-blue-500 text-white w-20 h-8 rounded-3xl' onClick={() => edite_func(data.id)}>Edite</button>
-                                </Link>
-                              </div>
+        <div className='container'>
+          <Row className=''>
+            {name.filter((res) => {
+              console.log(res.title);
+              return res.title.toLocaleLowerCase() === "" ? value : res.title.toLocaleLowerCase().includes(value)
+            })
+              .map((data) => {
+                return (
+                  <>
+                  <Col >
+                    <tbody>
+                      <tr className=' mt-9' >
+                        <div className='bg-blue-200 mt-6 w-[320px] h-[320px] rounded-[30px] mb-[1px]' key={data.id}>
+                          <div className=' flex-wrap m-9'>
+                            <th className='' >#:</th>
+                            <td  >{data.id}</td>
+                            <h r />
+                            <th >title: </th>
+                            <td  >{data.title}</td>
+                            <h r />
+                            <th >price: </th>
+                            <td  >{data.price}</td>
+                            <h r />
+                            <th >author: </th>
+                            <td >{data.author}</td>
+                            <h r />
+                            <th  >img:
+                            </th>
+                            <img className='w-[90px] h-[90px]' src={data.img} alt="" />
+                            <div className="flex gap-3 mt-2 ">
+                              <button className='bg-blue-500 text-white w-20 h-8 rounded-3xl' onClick={() => delete_func(data.id)}>delet</button>
+                              <Link to={`/table/editetable/${data.id}`}>
+                                <button className='bg-blue-500 text-white w-20 h-8 rounded-3xl' onClick={() => edite_func(data.id)}>Edite</button>
+                              </Link>
                             </div>
                           </div>
-                        </tr>
-                      </tbody>
-                    </>
-                  )
-                })}
-            </div>
-          </div>
+                        </div>
+                      </tr>
+                    </tbody>
+                  </Col>
+                  </>
+                )
+              })}
+          </Row>
         </div>
       </Container>
     </div>
